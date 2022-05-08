41fe229b940858e618643df303bfea40b8fda6b7
andreluiz27 <52846311+andreluiz27@users.noreply.github.com>
2022-05-08 20:14:53 -0300

    Update settings.py

--- soccer_bot/settings.py
+++ soccer_bot/settings.py
@@ -169,6 +169,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
      ]
 }
